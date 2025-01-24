1148ed92a1144bcee8408e6cf26e685d5bf96c7b
Pedro H. <pemirador98@gmail.com>
2025-01-24 23:09:45 +0000

    pathfind fixed

--- tilemaps/spr_tile_itch.gd
+++ tilemaps/spr_tile_itch.gd
@@ -1,12 +1,12 @@
 extends TileMap
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _use_tile_data_runtime_update(layer, coords):
-	if coords in get_used_cells_by_id(3):
+	if coords in get_used_cells_by_id(2):
 		return true
 	return false
 	
 	
 func _tile_data_runtime_update(layer, coords, tile_data):
-	tile_data.set_navigation_polygon(2, null)
+	tile_data.set_navigation_polygon(0, null)
