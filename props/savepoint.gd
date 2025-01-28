44e62a63187dada88410fd0b55dac922c69054bf
Pedro H. <pemirador98@gmail.com>
2025-01-28 21:08:23 +0000

    add some save points

---
+++ props/savepoint.gd
@@ -0,0 +1,37 @@
+extends StaticBody2D
+@onready var label = $Label
+var player_in_area = false
+var key = "beterruba"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	label.visible = false
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player_in_area and Input.is_action_just_pressed("attack"):
+		label.text = "Progresso salvo!"
+		var config = ConfigFile.new()
+		config.set_value("Scenes", "Name", get_tree().current_scene.scene_file_path)
+		config.save_encrypted_pass("res://save/scenes.cfg", key)
+		print("salvo")
+	
+	
+	
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "player":
+		label.visible = true
+		player_in_area = true
+		
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	label.visible = false
+	player_in_area = false
+	pass # Replace with function body.
